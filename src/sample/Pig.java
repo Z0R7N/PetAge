b23d603b7991ad5cbd9bf85eb045d55651019626
Z0R7N <dmtrii.a@gmail.com>
2020-01-23 08:59:19 +0300

    working app

--- src/sample/Pig.java
+++ src/sample/Pig.java
@@ -6,11 +6,12 @@ public class Pig extends AnimalCat {
         double y = 0; //значение для человеческого возраста
         double x = getAge(); // текущее значение возраста кошки
         if (x >= 1.5) {
-            y = 2;
+            y = 0.0003 * (x * x) + 0.0610 * x + 0.9351;
         }
         if (x < 1.5 && x != 0) {
-            y = 1;
+            y = (-0.0021 * (x * x)) + 1.5980 * x + (-17.4975);
         }
+        System.out.println("pig");
         int humanAge = (int) Math.round(y);
         ControllerCounting.setHumanAge(humanAge);
         runWindowCount();
